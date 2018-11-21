3e04e7393a4bb3cd36f4fd98edbaa730976eb024
Pedro Branco <brankix@gmail.com>
2018-11-21 22:27:53 +0000

    Get firebase expenses on start

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test'} );
-} else if (process.env.NODE_ENV === 'development ') {
+} else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development'} );
 }
 
