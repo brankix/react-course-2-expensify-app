b9e4aea445177f388cf5a0c01c2b748a20d9ec52
Pedro Branco <brankix@gmail.com>
2018-11-22 23:46:21 +0000

    Private Route

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -13,14 +13,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 // import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
   // const wrapper = shallow(<Header />);
   // expect(wrapper.find('h1').length).toBe(1);
   // expect(wrapper.find('h1').text()).toBe("Expensify");
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => { }}/>);
   expect(wrapper).toMatchSnapshot();
   // using import....
   // expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout}/>);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
